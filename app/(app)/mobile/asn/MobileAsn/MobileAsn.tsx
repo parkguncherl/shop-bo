7e5ae4de3b16c8b5ad900c8e10481e98163f9a5d
parkguncherl <luckeey21@gmail.com>
2026-01-12 18:13:14 +0900

    mogile

--- app/(app)/mobile/asn/MobileAsn/MobileAsn.tsx
+++ app/(app)/mobile/asn/MobileAsn/MobileAsn.tsx
@@ -11,35 +11,35 @@ const MobileAsn: React.FC = () => {
   // 세션 정보
   const session = useSession();
   return (
-    <div className={styles.container}>
-      <nav className={styles.nav}>
-        <button className={styles.navButton}>프필트</button>
-        <button className={styles.navButton}>김예솔</button>
-        <button className={styles.navButton}>경번사</button>
+    <div>
+      <nav>
+        <button>프필트</button>
+        <button>김예솔</button>
+        <button>경번사</button>
       </nav>
 
-      <div className={styles.search}>
-        <input type="text" placeholder="김예솔" className={styles.searchInput} />
+      <div>
+        <input type="text" placeholder="김예솔" />
       </div>
 
-      <main className={styles.main}>
-        <button className={styles.mainButtonActive}>경번사</button>
-        <button className={styles.mainButton}>생산처 2</button>
-        <button className={styles.mainButton}>생산처 3</button>
-        <button className={styles.mainButton}>생산처 4</button>
-        <button className={styles.mainButton}>생산처 5</button>
-        <button className={styles.mainButton}>생산처 6</button>
-        <button className={styles.mainButton}>생산처 7</button>
-        <button className={styles.mainButton}>생산처 8</button>
-        <button className={styles.mainButton}>생산처 9</button>
-        <button className={styles.mainButton}>생산처 10</button>
-        <div className={styles.dots}>...</div>
-        <button className={styles.loadMore}>생물보기 </button>
+      <main>
+        <button>경번사</button>
+        <button>생산처 2</button>
+        <button>생산처 3</button>
+        <button>생산처 4</button>
+        <button>생산처 5</button>
+        <button>생산처 6</button>
+        <button>생산처 7</button>
+        <button>생산처 8</button>
+        <button>생산처 9</button>
+        <button>생산처 10</button>
+        <div>...</div>
+        <button>생물보기 </button>
       </main>
 
-      <footer className={styles.footer}>
-        <button className={styles.footerButton}>일반 발주</button>
-        <button className={styles.footerButton}>수산 발주</button>
+      <footer>
+        <button>일반 발주</button>
+        <button>수산 발주</button>
       </footer>
     </div>
   );
