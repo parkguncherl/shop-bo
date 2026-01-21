3c695d1013e459d418c47d4cbd5cd370151db6fb
Park Junsung <junsungpark774@gmail.com>
2026-01-21 16:25:18 +0900

    금일 마무리, react hook form controller 영역의 관할을 적절히 지정하기

--- components/FormEnhancedTextArea.tsx
+++ components/FormEnhancedTextArea.tsx
@@ -2,7 +2,7 @@ import { BaseTextAreaAtom, BaseTextAreaAtomProps } from './atom/BaseTextAreaAtom
 import { FieldValues, useController } from 'react-hook-form';
 import { TControl } from '../types/Control';
 import mergeRefs from '../customFn/ref/mergeRefs';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export interface UploadRequestInterruptEvent {
   files: File[];
@@ -16,7 +16,7 @@ type FormEnhancedTextAreaProps<T extends FieldValues> = BaseTextAreaAtomProps &
 interface ContentElement {
   id: number; // 기본 1부터 시작
   partialContent?: string; // 이미지인 경우 undefined
-  frozen?: boolean; // 수동 통제, 기본적으로 최하단 요소 이외에는 true 이나 상호작용(수정을 위한 포커싱 등) 발생 시에는 예외적으로 조정 가능(다만 불변성 유지 차원에서 새 배열 생성 후 상태 최신화), 이미지인 경우 undefined
+  //frozen?: boolean; // 수동 통제, 기본적으로 최하단 요소 이외에는 true 이나 상호작용(수정을 위한 포커싱 등) 발생 시에는 예외적으로 조정 가능(다만 불변성 유지 차원에서 새 배열 생성 후 상태 최신화), 이미지인 경우 undefined
 }
 
 /**
@@ -24,6 +24,7 @@ interface ContentElement {
  * 기존 textArea 와 달리 이미지 삽입 및 이에 따라 필요한 동작 지원
  * */
 const FormEnhancedTextArea = <T extends FieldValues>({ control, rules, name, ref, ...props }: FormEnhancedTextAreaProps<T>) => {
+  /** react hook form 의 controller 는 현재 영역에서는 수정 대상 영역의 값(contentElement)에 한정되어 적용함(전역 적용하지 아니함) */
   const {
     field: { value, onChange: controlChange, ref: refForUseController },
     fieldState: { isDirty, isTouched, error },
@@ -31,12 +32,18 @@ const FormEnhancedTextArea = <T extends FieldValues>({ control, rules, name, ref
 
   /** 해당 지역 상태는 반드시 배열의 불변성을 유지할 것 */
   const [contentElements, setContentElements] = useState<ContentElement[]>([
-    { id: 1, partialContent: 'ddsds', frozen: true },
-    { id: 2, partialContent: '', frozen: false },
+    { id: 1, partialContent: 'ddsds' },
+    { id: 2, partialContent: '' },
   ]);
+  const [unFrozenElementId, setUnFrozenElementId] = useState<number>(-1);
 
   /** 기타 state */
   const [enableCompletionInterruptCallback, setEnableCompletionInterruptCallback] = useState(true); // 작성 완료 콜백의 무분별한 호출을 제한하는 상태
+
+  useEffect(() => {
+    // contentElements 상태에 따라 frozen 대상 요소의 id 동기화
+    setUnFrozenElementId(contentElements[contentElements.length - 1] ? contentElements[contentElements.length - 1].id : 1);
+  }, [contentElements]);
 
   // 업로드(파일, 이미지) 요청 동작 핸들링
   const uploadRequestInterruptCallBack = (files: File[]): Promise<void> | undefined => {
@@ -84,7 +91,34 @@ const FormEnhancedTextArea = <T extends FieldValues>({ control, rules, name, ref
     <div className={'enhanced_textArea'}>
       <div className={'formBox'}>
         {contentElements.map((contentElement) => {
-          if (contentElement.frozen) {
+          if (contentElement.id == unFrozenElementId) {
+            return (
+              <div className={'per_content_element'} key={contentElement.id}>
+                <BaseTextAreaAtom
+                  //value={contentElement.partialContent}
+                  type={'text'}
+                  ref={mergeRefs<HTMLTextAreaElement>(ref, refForUseController)}
+                  onChange={(e) => {
+                    setContentElements((prevState) => {
+                      return prevState.map((prev) => {
+                        if (prev.id == contentElement.id) {
+                          return {
+                            ...prev,
+                            partialContent: e.target.value,
+                          };
+                        } else {
+                          return prev;
+                        }
+                      });
+                    });
+                  }}
+                  onDrop={(e) => onDropEventHandler(e)}
+                  onPaste={(e) => onPasteEventHandler(e)}
+                />
+              </div>
+            );
+          } else {
+            // frozen(편집 제한)
             return (
               <div className={'per_content_element'} key={contentElement.id}>
                 <BaseTextAreaAtom
@@ -93,20 +127,6 @@ const FormEnhancedTextArea = <T extends FieldValues>({ control, rules, name, ref
                   onDrop={(e) => onDropEventHandler(e)}
                   onPaste={(e) => onPasteEventHandler(e)}
                   disabled={true}
-                />
-              </div>
-            );
-          } else {
-            return (
-              <div className={'per_content_element'} key={contentElement.id}>
-                <BaseTextAreaAtom
-                  //value={contentElement.partialContent}
-                  value={value || ''} // todo 추후 적절한 상태에 의존토록 하기
-                  type={'text'}
-                  ref={mergeRefs<HTMLTextAreaElement>(ref, refForUseController)}
-                  onChange={controlChange}
-                  onDrop={(e) => onDropEventHandler(e)}
-                  onPaste={(e) => onPasteEventHandler(e)}
                 />
               </div>
             );
