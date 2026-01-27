c94307f456f6f6fe63961bea734a33ea7fe527ba
park jun sung <junsungpark774@gmail.com>
2026-01-27 09:27:06 +0900

    antd 제공 provider 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import '../styles/global.scss';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import { AntdRegistry } from '@ant-design/nextjs-registry';
 
 /**
  * (server side)RootLayout
@@ -24,7 +25,9 @@ const layout = ({ children }: { children: React.ReactNode }) => {
         {/* todo PrintScripts 는 추후 해당 스크립트를 요하는 컴포넌트 각각에서의 사용이 더 안전, 현재는 기존 page 라우팅과 유사하게 전역적으로 선언하였으나 추후 리팩터링 하여야 */}
         {/*<PrintScripts />*/}
         <RootProvider>
-          <GlobalErrorBoundary>{children}</GlobalErrorBoundary>
+          <GlobalErrorBoundary>
+            <AntdRegistry>{children}</AntdRegistry>
+          </GlobalErrorBoundary>
         </RootProvider>
       </body>
     </html>
